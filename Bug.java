eb75821eaa85642d3ba7ed26d934c41800b945d6
BugHunter-X <askzenai@gmail.com>
2024-12-11 05:30:05 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,12 @@
+public class UncommonBug {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+
+        // This will throw an ArithmeticException
+        int z = x / y; 
+
+        System.out.println(z); // this line will not be reached
+    }
+}
